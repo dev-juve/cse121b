8cc4e08ac210dad4ad1fa93eca94447770adc8c5
dev-juve <juvenson101@gmail.com>
2024-01-30 22:05:29 -0500

    W03 assignment done.

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -8,20 +8,20 @@ function add(number1, number2){
 }
 
 function addNumbers(){
-    let addNmuber1 = Number(document.querySelector("#add1".value));
-    let addNmuber2 = Number(document.querySelector("#add2".value));
+    let addNmuber1 = Number(document.querySelector("#add1").value);
+    let addNmuber2 = Number(document.querySelector("#add2").value);
     document.querySelector("#sum").value = add(addNmuber1, addNmuber2);
 }
 document.querySelector("#addNumbers").addEventListener("click", addNumbers);
 /* Function Expression - Subtract Numbers */
 const subtract = function(number1, number2){
-    return number1 - number2;  
+    return (number1 - number2);  
 }
 
 const subtractNumbers = function(){
-    let subtractNmuber1 = Number(document.querySelector("#subtract1".value));
-    let subtractNmuber2 = Number(document.querySelector("#subtract2".value));
-    document.querySelector("#difference").value = add(subtractNmuber1, subtractNmuber2);
+    let subtractNmuber1 = Number(document.querySelector("#subtract1").value);
+    let subtractNmuber2 = Number(document.querySelector("#subtract2").value);
+    document.querySelector("#difference").value = subtract(subtractNmuber1, subtractNmuber2);
 }
 document.querySelector("#subtractNumbers").addEventListener("click", subtractNumbers);
 
@@ -32,9 +32,9 @@ const multiply = function(number1, number2){
 }
 
 const multiplyNumbers = () => {
-    let multiplyNmuber1 = Number(document.querySelector("#factor1".value));
-    let multiplyNmuber2 = Number(document.querySelector("#factor2".value));
-    document.querySelector("#product").value = add(multiplyNmuber1, multiplyNmuber2);
+    let multiplyNumber1 = Number(document.querySelector("#factor1").value);
+    let multiplyNumber2 = Number(document.querySelector("#factor2").value);
+    document.querySelector("#product").value = multiply(multiplyNumber1, multiplyNumber2);
 }
 document.querySelector("#multiplyNumbers").addEventListener("click", multiplyNumbers);
 
@@ -45,9 +45,9 @@ function divide(number1, number2){
 }
 
 function divideNumbers(){
-    let divideNmuber1 = Number(document.querySelector("#dividend".value));
-    let divideNmuber2 = Number(document.querySelector("#divisor".value));
-    document.querySelector("#quotient").value = add(divideNmuber1, divideNmuber2);
+    let divideNumber1 = Number(document.querySelector("#dividend").value);
+    let divideNumber2 = Number(document.querySelector("#divisor").value);
+    document.querySelector("#quotient").value = divide(divideNumber1, divideNumber2);
 }
 document.querySelector("#divideNumbers").addEventListener("click", divideNumbers);
 
